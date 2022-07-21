71ad99ff684f843e7aa56bfe69a1451634d304d7
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-07-21 13:53:21 +0300

    home page case start

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,18 @@
+<script lang="ts" setup>
+import HeaderBar from './components/HeaderBar.vue';
+
+
+</script>
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <HeaderBar/>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
-</script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container{
+  width: 90%;
+  max-width: 1250px;
+  margin: 0px auto;
 }
 </style>
---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,25 @@
+
+<script lang="ts" setup>
+
+
+
+</script>
+
+<template>
+<header>
+  <div class="container">
+    <div class="logo">
+      <img src="https://kommunity.com/img/logo.svg" loading="lazy" alt="">
+    </div>
+  </div>
+</header>
+</template>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+header{
+  background:gray; 
+  height: 100px;
+}
+</style>
